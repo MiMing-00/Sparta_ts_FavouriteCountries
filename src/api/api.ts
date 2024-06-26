503840072434e6ab86c35f69389a351c3dcec0a1
Sera Choi <c.sera2511@gmail.com>
2024-06-26 19:35:48 +0900

    [style] : diff selected or not

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,7 +6,7 @@ export const fetchData = async (): Promise<CountryArray> => {
   // try {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
-  // console.log(data);
+  console.log(data);
   return data;
   // } catch (error) {
   //   console.log(error);
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -19,7 +19,7 @@ const CountryList = () => {
 
   const handleOnclickEvent = (event: React.FormEvent<HTMLLIElement>) => {
     const theCountryId = event.currentTarget.id;
-    const index = data.findIndex((item) => item.ccn3 === theCountryId);
+    const index = data.findIndex((item) => item.cca2 === theCountryId);
     const theCountry = data[index];
 
     if (index === -1) {
@@ -28,24 +28,18 @@ const CountryList = () => {
       return;
     }
 
-    // 클릭된 애 => "선택된 나라" = data[index]
-    // setSelectedCountry에서 "선택된 나라"가 있을 경우 (*findIndex?) 삭제 - 필터로 제외 : 비교할 거 data[index].ccn3 와 filter에서 item.ccn3랑 비교
-    // setSelectedCountry에서 "선택된 나라"가 없을 경우 포함 + 넣기
-
     const theCountryIndex = selectedCountry.findIndex(
-      (item) => item.ccn3 === theCountry.ccn3
+      (item) => item.cca2 === theCountry.cca2
     );
 
-    //셀렉티드에 없을 경우, 추가 + 데이터에서는 삭제
     if (theCountryIndex === -1) {
       setSelectedCountry((prev) => [theCountry, ...prev]);
       setAllCountry((prev) =>
-        prev.filter((item) => item.ccn3 !== theCountry.ccn3)
+        prev.filter((item) => item.cca2 !== theCountry.cca2)
       );
-      //셀렉티드에 있을 경우 삭제 + 데이터에서는 추가
     } else {
       setSelectedCountry((prev) =>
-        prev.filter((item) => item.ccn3 !== theCountry.ccn3)
+        prev.filter((item) => item.cca2 !== theCountry.cca2)
       );
       setAllCountry((prev) => [theCountry, ...prev]);
     }
@@ -54,28 +48,29 @@ const CountryList = () => {
   return (
     <div>
       {/* 맵으로 뿌리기 */}
-      <ul>
-        <h2>선택된 나라</h2>
+      <h2 className="font-bold">선택된 나라</h2>
+      <ul className="grid xl:grid-cols-4 gap-4 md:grid-cols-3 gap-3 sm:grid-cols-2 gap-2">
         {selectedCountry?.map((country) => (
           <li
             key={country.cca2}
             onClick={handleOnclickEvent}
-            className="border-4 my-4 bg-slate-100"
-            id={country.ccn3}
+            className="border-4 my-4 bg-emerald-100 h-[80px] truncate cursor-pointer"
+            id={country.cca2}
           >
             <CountryCard country={country} />
           </li>
         ))}
       </ul>
-      <ul>
-        {/* 생각해보니 셀렉트 됐으면 여기에 안 나와야 한다. */}
-        <h2>모든 나라</h2>
+      <br />
+      {/* 생각해보니 셀렉트 됐으면 여기에 안 나와야 한다. */}
+      <h2 className="font-bold">모든 나라</h2>
+      <ul className="grid xl:grid-cols-4 gap-4 md:grid-cols-3 gap-3 sm:grid-cols-2 gap-2">
         {allCountry?.map((country) => (
           <li
             key={country.cca2}
             onClick={handleOnclickEvent}
-            className="border-4 my-4 bg-slate-100"
-            id={country.ccn3}
+            className="border-4 my-4 bg-indigo-100  h-[80px] truncate cursor-pointer"
+            id={country.cca2}
           >
             <CountryCard country={country} />
           </li>
